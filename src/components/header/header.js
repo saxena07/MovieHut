0898e79af6e9932232cef9b65843460e865b4fad
saxena07 <saxenalakshya125@gmail.com>
2022-07-25 00:28:40 +0530

    Header Changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,9 +8,9 @@ const Header = () => {
     return (<div className='header'>
         <div className='headerleft'>
             <Link to="/"><img className='header__icon' alt="img" src="/mimg.png" /></Link>
-            <Link to="/movies/popular"  style={{textDecoration : "none"}}><span>Popular</span></Link>
-            <Link to="/movies/top_rated" style={{textDecoration : "none"}} ><span>Top Rated</span></Link>
-            <Link to="/movies/upcoming" style={{textDecoration : "none"}}><span>Upcoming</span></Link>
+            <Link to="/movies/popular"  style={{textDecoration : "none"}}><span>POPULAR</span></Link>
+            <Link to="/movies/top_rated" style={{textDecoration : "none"}} ><span>TOP-RATED</span></Link>
+            <Link to="/movies/upcoming" style={{textDecoration : "none"}}><span>UPCOMING</span></Link>
             <span className='searchbar'>
             <form target={<Link to="/movies/upcoming" />}>
             <input className="searchbarin" type="text" placeholder=" Enter movie name " />
